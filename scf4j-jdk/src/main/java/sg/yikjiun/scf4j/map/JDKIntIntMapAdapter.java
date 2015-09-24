d4a1b91f63d0203db17badb15c2a6e84c12c3e54
Lee Yik Jiun <lee.yikjiun@gmail.com>
2015-09-25 00:46:38 +0800

    Organized packages

---
+++ scf4j-jdk/src/main/java/sg/yikjiun/scf4j/map/JDKIntIntMapAdapter.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 Lee Yik Jiun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sg.yikjiun.scf4j.map;
+
+import java.util.Map;
+
+/**
+ * @author Lee Yik Jiun
+ */
+public class JDKIntIntMapAdapter implements IntIntMap {
+    private final Map<Integer, Integer> map;
+
+    public JDKIntIntMapAdapter(Map<Integer, Integer> map) {
+        this.map = map;
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public boolean containsKey(int key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsValue(int value) {
+        return map.containsValue(value);
+    }
+
+    public int get(int key) {
+        return map.get(key);
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public int put(int key, int value) {
+        return map.put(key, value);
+    }
+
+    public int remove(int key) {
+        return map.remove(key);
+    }
+
+    public int size() {
+        return map.size();
+    }
+}
---
+++ scf4j-jdk/src/main/java/sg/yikjiun/scf4j/set/impl/IntSetFactoryAdapter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Lee Yik Jiun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sg.yikjiun.scf4j.set.impl;
+
+import sg.yikjiun.scf4j.set.IntSet;
+import sg.yikjiun.scf4j.set.JDKIntSetFactory;
+
+/**
+ * @author Lee Yik Jiun
+ */
+public class IntSetFactoryAdapter {
+    private static final IntSetFactoryAdapter instance = new IntSetFactoryAdapter();
+
+    private static final JDKIntSetFactory factory = new JDKIntSetFactory();
+
+    private IntSetFactoryAdapter() {
+
+    }
+
+    public static IntSetFactoryAdapter getInstance() {
+        return instance;
+    }
+
+    public IntSet createIntSet() {
+        return factory.createIntHashSet();
+    }
+}
